f786bf1f04abada3b119900818ccc5cfbfaf7232
fortesnow <fortesnow@gmail.com>
2025-03-21 15:51:49 +0900

    Next.jsルートパラメータの使用方法を修正

--- app/members/[id]/page.tsx
+++ app/members/[id]/page.tsx
@@ -9,12 +9,17 @@ type Props = {
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // paramsオブジェクト自体をawaitする必要はありませんが、
+  // 必要に応じてここでidに基づいたデータ取得処理をawaitすることも可能です
+  const id = params.id;
   return {
-    title: `会員詳細 - ${params.id}`,
+    title: `会員詳細 - ${id}`,
   }
 }
 
 // ページコンポーネント（サーバーコンポーネント）
 export default async function MemberDetailPage({ params }: Props) {
-  return <MemberDetailClient id={params.id} />;
+  // paramsからidを取得して使用
+  const id = params.id;
+  return <MemberDetailClient id={id} />;
 }
